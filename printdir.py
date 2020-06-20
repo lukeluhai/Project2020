851f2e8b4956efcd8b80d664f670525107bd5f12
lukeluhai <35003761+lukeluhai@users.noreply.github.com>
2020-06-20 23:17:26 +0800

    新增printdir

---
+++ printdir.py
@@ -0,0 +1,16 @@
+import os
+
+f=open('E:\\KanKan\\VaCache\\aa.txt','w',encoding='utf-8')
+
+def printFile(path,name):
+    if os.path.isfile(path+'\\'+name):
+        f.writelines(name+'||'+path+'\n')
+
+    else:
+        for i in os.listdir(path+'\\'+name):
+            printFile(path+'\\'+name,i)
+    return
+
+
+printFile(r'E:\KanKan\VaCache\movie','潘多拉')
+f.close()
